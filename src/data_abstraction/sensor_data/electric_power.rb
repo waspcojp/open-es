870d7e26ffcb4ee26d92185dfe16c8beb82a6648
Masami Ogoshi <ogochan@wasp.co.jp>
2017-07-15 18:32:00 +0900

    new sensor_data

--- src/data_abstraction/sensor_data/electric_power.rb
+++ src/data_abstraction/sensor_data/electric_power.rb
@@ -1,12 +1,12 @@
 module DataAbstraction::SensorData
   class ElectricPower < Generic
-    STANDARD_UNIT = "KWh"
+    STANDARD_UNIT = "KW"
     def initialize(data, meta_values = {}, unit = STANDARD_UNIT)
       super(data, meta_values, unit)
-      @value = EnergyValue.new(data['value'].to_f, @unit)
+      @value = PowerValue.new(data['value'].to_f, @unit)
     end
     def self.unit_class
-      EnergyValue
+      PowerValue
     end
     def self.standard_unit
       STANDARD_UNIT
@@ -16,7 +16,7 @@ module DataAbstraction::SensorData
               'data_class_name' => self.name.split('::').last,
               'data' => {
                 'value' => 25,
-                'unit' => 'KWh' }
+                'unit' => 'KW' }
             })
     end
   end
