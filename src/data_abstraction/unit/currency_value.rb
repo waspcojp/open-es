782c4afd8a8244dcff63f15c8eae31433114cb22
Masami Ogoshi <ogochan@wasp.co.jp>
2017-08-03 18:43:16 +0900

    new sensor

--- src/data_abstraction/unit/currency_value.rb
+++ src/data_abstraction/unit/currency_value.rb
@@ -2,7 +2,38 @@ module DataAbstraction::Unit
   class CurrencyValue < Generic
     STANDARD_UNIT = "JPY"
     UNITS = [
+             [ "GBP", "GBP", "gbp", "Pound", "pound" ],
+             [ "ILS", "ILS", "ils" ],
+             [ "INR", "INR", "inl" ],
+             [ "IDR", "IDR", "idr" ],
+             [ "AUD", "AUD", "aud" ],
+             [ "CAD", "CAD", "cad" ],
+             [ "SGD", "SGD", "sgd" ],
+             [ "CHF", "CHF", "chf" ],
+             [" SEK", "SEK", "sek" ],
+             [ "THB", "THB", "thb" ],
+             [ "CZK", "CZK", "czk" ],
+             [ "CLP", "CLP", "clp" ],
+             [ "DKK", "DKK", "dkk" ],
+             [ "TRY", "TRY", "try" ],
+             [ "NZD", "NZD", "nzd" ],
+             [ "NOK", "NOK", "nok" ],
+             [ "PKR", "PKR", "pkr" ],
+             [ "HUF", "HUF", "huf" ],
+             [ "PHP", "PHP", "php" ],
+             [ "BRL", "BRL", "brl" ],
+             [ "PLN", "PLN", "pln" ],
+             [ "MYR", "MYR", "myr" ],
+             [ "MXN", "MXN", "mxn" ],
+             [ "EUR", "EUR", "eur", "Euro", "euro" ],
+             [ "RUB", "RUB", "rub" ],
+             [ "CNY", "CNY", "cny" ],
+             [ "ZAR", "ZAR", "zar" ],
+             [ "TWD", "TWD", "twd" ],
              [ "JPY", "JPY", "jpy", "Yen", "yen" ],
+             [ "USD", "USD", "usd", "Doller", "doller" ],
+             [ "KRW", "KRW", "krw", "Won", "won" ],
+             [ "HKD", "HKD", "hkd" ]
             ]
     @@unit_table = unit_table(UNITS)
 
