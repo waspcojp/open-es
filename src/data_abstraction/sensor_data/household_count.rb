032e8638ce45c4e3f993f92477d19f7a2d8b2394
mayumi <mayumi@wasp.co.jp>
2017-09-28 15:45:01 +0900

    add data class

---
+++ src/data_abstraction/sensor_data/household_count.rb
@@ -0,0 +1,23 @@
+module DataAbstraction::SensorData
+  class HouseholdCount < Generic
+    STANDARD_UNIT = ""
+    def initialize(data, meta_values = {}, unit = STANDARD_UNIT)
+      super(data, meta_values, unit)
+      @value = CountValue.new(data['value'].to_i, @unit)
+    end
+    def self.unit_class
+      CountValue
+    end
+    def self.standard_unit
+      STANDARD_UNIT
+    end
+    def self.dummy
+      super({
+              'data_class_name' => self.name.split('::').last,
+              'data' => {
+                'value' => 20
+              }
+            })
+    end
+  end
+end
